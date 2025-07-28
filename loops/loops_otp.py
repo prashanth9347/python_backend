3874ba3fea771abf11eb1f05d1185ad872aa65c9
prashanth9347 <157875646+prashanth9347@users.noreply.github.com>
2025-07-28 21:11:48 +0530

    Update loops_otp.py

--- loops/loops_otp.py
+++ loops/loops_otp.py
@@ -5,9 +5,9 @@ import random
 otp=random.randint(1000,9999)
 print(otp)
 
-attempts=3
+attempts=0
 
-while attempts:
+while attempts<=3:
         user_otp=int(input("enter the otp:"))
         if len(str(user_otp))!=4:
             print("otp must be 4 digits")
@@ -15,6 +15,7 @@ while attempts:
         if user_otp==otp:
             print("otp successfully entered") 
             break
-        attempts-=1
-else :
+        attempts+=1
+        if attempts==3:
             print("maximum attempts done ,try after 24 hours")  
+            break
